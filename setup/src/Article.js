d6d92fedd6705fe9ee6ffc5a520dfa0c5debadf9
Dino Hadzic <zgdino@yahoo.com>
2022-05-16 18:09:30 +0200

    setup landing page

--- setup/src/Article.js
+++ setup/src/Article.js
@@ -5,7 +5,7 @@ const Article = ({title, snippet, date, length}) => {
   return <article className="post">
     <h2>{title}</h2>
     <div className="post-info">
-      <span>{date}</span>
+      <span>date</span>
       <span>{length} min read</span>
     </div>
     <p>{snippet}</p>
