cd3b6d45df96da79040f21a779d8c10659c67469
Dino Hadzic <zgdino@yahoo.com>
2022-05-17 12:09:25 +0200

    toggle function and useEffect

--- setup/src/App.js
+++ setup/src/App.js
@@ -3,12 +3,27 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+  const [theme, setTheme] = useState('light-theme')
+
+  useEffect(() => {
+    document.documentElement.className = theme
+  }, [theme])
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    }
+    else{
+      setTheme('light-theme')
+    }
+  }
+
   return (
     <main>
       <nav>
         <div className='nav-center'>
           <h1>overreacted</h1>
-          <button className='btn'>toggle</button>
+          <button className='btn' onClick={toggleTheme}>toggle</button>
         </div>
       </nav>
       <section className='articles'>
